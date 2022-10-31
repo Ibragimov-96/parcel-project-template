28f42f7a466d5dad744a42a3bd10a24e470ad45c
Ibragimov-96 <dima.ibragimov.1996@ukr.net>
2022-10-31 12:37:10 +0200

    01

---
+++ src/js/03-promises.js
@@ -0,0 +1,35 @@
+import Notiflix from 'notiflix';
+const formData = document.querySelector('form');
+formData.addEventListener('submit', onFormSubmit);
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  let delayData = +formData.elements.delay.value;
+  const stepData = +formData.elements.step.value;
+  const amountData = +formData.elements.amount.value;
+  evt.target.reset();
+  for (let amount = 0; amount < amountData; amount++)
+    createPromise(amount + 1, (delayData += stepData))
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `:белая_галочка: Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `:х: Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  function createPromise(position, delay) {
+    const promise = new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, (delay -= stepData));
+    });
+    return promise;
+  }
+}
